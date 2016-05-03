c475368f43a9987c4c586d08a39a075e8945c436
Devon <djones9898@hotmail.com>
2016-05-02 21:03:46 -0800

    create project files for core classes

---
+++ CommandBuilder/Interfaces.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandBuilder.Interfaces
+{
+    //System Design
+    //- IBuilder 
+    //    - ReportBuilder 
+    //        - Build 
+    //        - BuildReport 
+    //    - CommandBuilder 
+    //        - Build 
+    //        - BuildCommand 
+    //        - BuildBuildProductCommand 
+    //        - BuildRenderProductCommand 
+    //        - BuildRunProductCommand 
+    //    - ValidationBuilder 
+    //        - Build 
+    //        - BuildValidation 
+    //- IProduct 
+    //    - Report 
+    //    - Command 
+    //    - Validation 
+    //- IDirector 
+    //    - ReportController 
+    //    - ValidatorController 
+    //- ICommand 
+    //    - BuildProductCommand 
+    //    - RenderProductCommand 
+    //    - RunProductCommand 
+    //- IReceiver 
+    //    - IBuilder 
+    //    - IProduct 
+    //- IInvoker 
+    //    - ReportController 
+    //    - ValidatorController
+
+
+    public interface IBuilder
+    {
+
+    }
+}
--- CommandBuilder/Products.cs
+++ CommandBuilder/Products.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CommandBuilder
 {
-    public class Class1
+    class Products
     {
     }
 }
